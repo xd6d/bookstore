6748c8d390a1949d3ea2f6481a479408e36ee38c
Artem Kurkin <kurkin566@gmail.com>
2024-04-12 21:19:13 +0300

    Refactored unit tests

--- server/src/test/java/com/example/bookstore/service/BookServiceTest.java
+++ server/src/test/java/com/example/bookstore/service/BookServiceTest.java
@@ -5,8 +5,7 @@ import com.example.bookstore.dao.BookRepository;
 import com.example.bookstore.mappers.BookMapper;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.utils.BookUtils;
-import io.grpc.inprocess.InProcessChannelBuilder;
-import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.internal.testing.StreamRecorder;
 import io.grpc.testing.GrpcCleanupRule;
 import org.junit.Rule;
 import org.junit.jupiter.api.AfterEach;
@@ -16,11 +15,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,12 +26,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class BookServiceTest {
 
     private static final Book EXAMPLE_BOOK = BookUtils.getDefaultBook();
+
     @Rule
     public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
     @Autowired
     private BookService bookService;
+
     @Autowired
     private BookRepository bookRepository;
+
     @Autowired
     private BookMapper bookMapper;
 
@@ -44,45 +45,55 @@ class BookServiceTest {
     }
 
     @Test
-    void bookCreateTest() throws IOException {
-        String serverName = InProcessServerBuilder.generateName();
-        grpcCleanup.register(InProcessServerBuilder
-                .forName(serverName).directExecutor().addService(bookService).build().start());
+    void bookCreateTest() throws Exception {
+        StreamRecorder<BookDto> responseObserver = StreamRecorder.create();
+        bookService.create(bookMapper.toRequest(EXAMPLE_BOOK), responseObserver);
 
-        BookServiceGrpc.BookServiceBlockingStub blockingStub = BookServiceGrpc.newBlockingStub(
-                grpcCleanup.register(InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+        assertNull(responseObserver.getError());
 
-        var response = blockingStub.create(bookMapper.toRequest(EXAMPLE_BOOK));
+        List<BookDto> results = responseObserver.getValues();
+        assertEquals(1, results.size());
+
+        var response = results.get(0);
+
         assertDoesNotThrow(() -> UUID.fromString(response.getId()), "ID have not been created");
         assertTrue(bookRepository.findById(UUID.fromString(response.getId())).isPresent(), "Book has not been created");
     }
 
     @Test
     @Sql("/book.sql")
-    void getAllTest() throws IOException {
-        String serverName = InProcessServerBuilder.generateName();
-        grpcCleanup.register(InProcessServerBuilder
-                .forName(serverName).directExecutor().addService(bookService).build().start());
+    void getAllTest() throws Exception {
+        StreamRecorder<BookDto> responseObserver = StreamRecorder.create();
 
-        BookServiceGrpc.BookServiceBlockingStub blockingStub = BookServiceGrpc.newBlockingStub(
-                grpcCleanup.register(InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+        bookService.getAll(bookMapper.toRequest(EXAMPLE_BOOK), responseObserver);
 
-        var response = blockingStub.getAll(bookMapper.toRequest(EXAMPLE_BOOK));
-        var responseList = toList(response);
-        assertEquals(1, responseList.size(), "Could not find specified book");
-        var found = bookMapper.toEntity(responseList.get(0));
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+        assertNull(responseObserver.getError());
+
+        List<BookDto> results = responseObserver.getValues();
+        assertEquals(1, results.size(), "Could not find specified book");
+
+
+        var found = bookMapper.toEntity(results.get(0));
         assertEquals(EXAMPLE_BOOK.getTitle(), found.getTitle(), "Titles should match");
         assertEquals(EXAMPLE_BOOK.getAuthor(), found.getAuthor(), "Authors should match");
         assertEquals(EXAMPLE_BOOK.getIsbn(), found.getIsbn(), "ISBNs should match");
         assertEquals(EXAMPLE_BOOK.getQuantity(), found.getQuantity(), "Quantities should match");
         assertEquals(EXAMPLE_BOOK, found, "Books should match");
-        responseList = toList(blockingStub.getAll(BookRequest.getDefaultInstance()));
-        assertEquals(bookRepository.findAll().size(), responseList.size(), "Entity amounts should match");
-    }
 
-    private <T> List<T> toList(Iterator<T> iterator) {
-        List<T> list = new LinkedList<>();
-        iterator.forEachRemaining(list::add);
-        return list;
+        responseObserver = StreamRecorder.create();
+        bookService.getAll(BookRequest.getDefaultInstance(), responseObserver);
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+        assertNull(responseObserver.getError());
+
+        results = responseObserver.getValues();
+        assertEquals(bookRepository.findAll().size(), results.size(), "Entity amounts should match");
     }
 }
